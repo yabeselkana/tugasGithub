d7c87bbb1ed85a24feaf7b75894607a2fa1e3dfa
yabeselkana <yabeszega1997@gmail.com>
2023-06-04 06:16:00 +0700

    pengurangan nilai pada file javascript yabes

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const nilai_b = 10;
 
 const nilai_c = nilai_a + nilai_b;
 
-const nilai_w = 5;
-const nilai_o = 10;
+const nilai_w = 10;
+const nilai_o = 50;
 
-const nilai_f = nilai_w + nilai_o;
+const nilai_f = nilai_w - nilai_o;
